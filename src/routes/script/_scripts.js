0760ad190795ddf9f8ee8fdd521570bd64d15013
Jack <git@5079.ml>
2021-07-27 00:50:51 -0400

    fix yessir shit code

--- src/routes/script/_scripts.js
+++ src/routes/script/_scripts.js
@@ -5,32 +5,34 @@ export const scripts = {
     name: 'Tux Autofarm',
     description: 'The fastest autofarm for Build A Boat For Treasure, powered by Tux.',
     source: dedent`
-    local Stages=workspace.BoatStages.NormalStages
-    local client=game:GetService("Players").LocalPlayer
-    local RunService=game:GetService("RunService")
+    local Stages = workspace.BoatStages.NormalStages
+    local client = game:GetService("Players").LocalPlayer
+    local RunService = game:GetService("RunService")
 
-    local Penguin,Gold=workspace.ChangeCharacter,workspace.ClaimRiverResultsGold
+    local Penguin = workspace.ChangeCharacter
+    local Gold = workspace.ClaimRiverResultsGold
 
-    _G.Busy=true;
+    _G.Busy = true
 
     function Float()
         while _G.Busy do
             RunServices.RenderStepped:wait()
-            if client.Character:FindFirstChild("Humanoid")~=nil then
+            if client.Character:FindFirstChild("Humanoid") ~= nil then
                 client.Character.Humanoid:ChangeState(10)
             end
         end
     end
-    
+
     function Farm()
         while _G.Busy do
-            for i=1,10 do wait(2)
-                client.Character.HumanoidRootPart.CFrame=Stages["CaveStage"..i].DarknessPart.CFrame
-                wait(0.1)
-                Gold:FireServer() end
+            for i = 1,10 do
+            wait(2)
+            client.Character.HumanoidRootPart.CFrame=Stages["CaveStage" .. i].DarknessPart.CFrame
+            wait(0.1)
+            Gold:FireServer() end
             wait(1)
             Penguin:FireServer("PenguinCharacter")
-            repeat wait() until client.Character:FindFirstChild("HumanoidRootPart")
+            client.Character:WaitForChild("HumanoidRootPart", math.huge)
         end
     end
 
