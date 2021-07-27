776f804448e40fefc12467d8e2382a160b6f9cb3
Jack <git@5079.ml>
2021-07-27 00:32:50 -0400

    add script list

---
+++ src/routes/scripts.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { scripts } from './script/_scripts'
+</script>
+
+<svelte:head>
+  <title>Scripts</title>
+</svelte:head>
+
+{#each Object.entries(scripts) as [id, script]}
+  <a href={`/script/${id}`}>
+    <article>
+      <h2>{script.name}</h2>
+      <h3>{script.description}</h3>
+    </article>
+  </a>
+{/each}
+
+<style>
+  a {
+    color: var(--heading-color);
+  }
+  a:hover {
+    text-decoration: none;
+  }
+</style>
