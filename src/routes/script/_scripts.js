4dc42eadf7149fc946e4c9fed7ca8865b3daeed2
Jack <git@5079.ml>
2021-07-27 00:48:37 -0400

    tux autofarm

--- src/routes/script/_scripts.js
+++ src/routes/script/_scripts.js
@@ -1,7 +1,35 @@
 export const scripts = {
-  [5079]: {
-    name: 'example script',
-    description: 'it does a funny',
+  [1]: {
+    name: 'Tux Autofarm',
+    description: 'The fastest autofarm for Build A Boat For Treasure, powered by Tux.',
+    source: `local Stages=workspace.BoatStages.NormalStages
+local client=game:GetService("Players").LocalPlayer
+local RunService=game:GetService("RunService")
+
+local Penguin,Gold=workspace.ChangeCharacter,workspace.ClaimRiverResultsGold
+
+_G.Busy=true;
+
+function Float()
+    while _G.Busy do game:GetService("RunService").RenderStepped:wait()
+        if client.Character:FindFirstChild("Humanoid")~=nil then
+            client.Character.Humanoid:ChangeState(10)
+        end
+    end
+end
+function Farm()
+    while _G.Busy do
+        for i=1,10 do wait(2)
+            client.Character.HumanoidRootPart.CFrame=Stages["CaveStage"..i].DarknessPart.CFrame
+            wait(0.1)
+            workspace.ClaimRiverResultsGold:FireServer() end
+        wait(1)
+        Penguin:FireServer("PenguinCharacter")
+        repeat wait() until client.Character:FindFirstChild("HumanoidRootPart")
+    end
+end
+
+spawn(Float) spawn(Farm)`,
     games: [537413528]
   }
 }
