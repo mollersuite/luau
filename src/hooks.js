234d58c461092187107a4a104a304209a4e9cee4
Jack <git@5079.ml>
2021-07-27 02:37:31 -0400

    scriptware sure is great

--- src/hooks.js
+++ src/hooks.js
@@ -2,6 +2,7 @@ export function getSession(request) {
   return {
     exploit: !!(
       request.headers['user-agent']?.startsWith('synx') ||
+      request.headers['user-agent']?.startsWith('ScriptWare') ||
       Object.keys(request.headers).find((key) => key.endsWith('-fingerprint'))
     )
   }
