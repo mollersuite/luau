753534436b50fcff2d7b5b611f05d33e16715f6c
Jack <git@5079.ml>
2021-07-27 23:42:08 -0400

    use node for now

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
 import workers from '@sveltejs/adapter-cloudflare-workers'
+import node from '@sveltejs/adapter-node'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: workers()
+    adapter: node(), //workers(),
+    vite: {}
   }
 }
 
