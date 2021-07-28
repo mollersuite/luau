4431c36d48ed56cd72612abb7b775ea702772ef6
Jack <git@5079.ml>
2021-07-27 23:48:39 -0400

    switch back to workers

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import workers from '@sveltejs/adapter-cloudflare-workers'
-import node from '@sveltejs/adapter-node'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: node(), //workers(),
+    adapter: workers(),
     vite: {}
   }
 }
