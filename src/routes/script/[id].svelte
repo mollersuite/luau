2710700eee3658711a18403428fac2177e7890d1
Jack <git@5079.ml>
2021-08-18 20:04:09 -0400

    dont use httpget for fallback

--- src/routes/script/[id].svelte
+++ src/routes/script/[id].svelte
@@ -55,12 +55,18 @@
 <script>
   import { user, supabase } from '$lib/supabase'
   import CodeButton from '$lib/CodeButton.svelte'
-
+  import dedent from 'dedent'
   /** @type {{name: string, id: string,source: string, description: string, user_id: string, games: {Name: string, AssetId: number}[]}} */
   export let script
   export let host
   export let id
-  let code = `loadstring(game:HttpGet("https://${host}/script/${id}"), ${JSON.stringify(script.name)})()`
+  let code = dedent`
+  getgenv().request = request or (http and http.request) or (syn and syn.request) or (lib and lib.request)
+  loadstring(request({
+    Url = "https://${host}/script/${id}",
+    Method = 'GET'
+  }).Body, ${JSON.stringify(script.name)})()`
+
   $: owner = script.user_id === $user?.id
   let name = script.name
   let desc = script.description
@@ -146,32 +152,29 @@
   </details>
   <details>
     <summary>What scripting utilities are supported by Luau.ml?</summary>
-    <del>
-      <ul>
-        <li>
-          <a href="https://script-ware.com">
-            <img src="/sw.svg" height="16" alt="Script-Ware logo" /> Script-Ware</a
-          >
-        </li>
-        <li>
-          <a href="https://x.synapse.to"
-            ><img src="/synx.svg" height="16" alt="Synapse X logo" /> Synapse X</a
-          >
-        </li>
-        <li>
-          <a href="https://krnl.ca" rel="nofollow"
-            ><img src="/krnl.svg" height="16" alt="Krnl logo" /> Krnl (not tested;
-            someone told me the user agent)</a
-          >
-        </li>
-        <li>Electron</li>
-        <li>
-          <a href="https://fluxteam.xyz" rel="nofollow">Fluxus</a>
-        </li>
-        <li>Sentinel (not tested; looked at API docs for user agent)</li>
-      </ul>
-    </del>
-    Pretty much every exploit.
+    <ul>
+      <li>
+        <a href="https://script-ware.com">
+          <img src="/sw.svg" height="16" alt="Script-Ware logo" /> Script-Ware</a
+        >
+      </li>
+      <li>
+        <a href="https://x.synapse.to"
+          ><img src="/synx.svg" height="16" alt="Synapse X logo" /> Synapse X</a
+        >
+      </li>
+      <li>
+        <a href="https://krnl.ca" rel="nofollow"
+          ><img src="/krnl.svg" height="16" alt="Krnl logo" /> Krnl (not tested;
+          someone told me the user agent)</a
+        >
+      </li>
+      <li>Electron</li>
+      <li>
+        <a href="https://fluxteam.xyz" rel="nofollow">Fluxus</a>
+      </li>
+      <li>Sentinel (not tested; looked at API docs for user agent)</li>
+    </ul>
   </details>
 {/if}
 
