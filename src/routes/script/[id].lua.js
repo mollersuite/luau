57c39708d59e63fb922e2732466755b21d76c881
Jack <git@5079.ml>
2021-08-24 18:33:02 -0400

    its only ever gonna be used with one input

--- src/routes/script/[id].lua.js
+++ src/routes/script/[id].lua.js
@@ -39,13 +39,9 @@ export async function get({ params, headers }) {
       do
         local games = { ${script.games.join(', ')} }
         if not table.find(games, game.PlaceId) then -- place id NOT game id
-          -- validnumber function and random string polyfill by jac (not jack)
-          local function ValidNumber(Number)
-            return type(Number) == "number" and not string.find(string.lower(tostring(Number)),"nan")
-          end
-          
+          -- random string polyfill by jac (not jack)
           local function RandomString(Length)
-              return string.gsub(string.rep(" ",ValidNumber(Length) and math.clamp(Length,1,2e5-1) or math.random(5,100)),".",function()
+              return string.gsub(string.rep(" ",Length,".",function()
                   return string.char(({math.random(48,57),math.random(65,90),math.random(97,122)})[math.random(1,3)])
               end)
           end
