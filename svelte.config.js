26ff2fed2339fa5c56634f7ee3b09d6ebd559082
Jack <git@5079.ml>
2021-10-01 15:07:26 -0400

    no more #svelte!

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,7 @@ import workers from '@sveltejs/adapter-cloudflare-workers'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
     adapter: workers(),
-    vite: {},
     files: {
       lib: 'lib',
       routes: 'routes',
