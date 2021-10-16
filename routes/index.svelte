53974f59d596a24f3d6fae487a2490aa1193a8c2
Jack <git@5079.ml>
2021-10-16 15:36:57 -0400

    better

--- routes/index.svelte
+++ routes/index.svelte
@@ -51,76 +51,30 @@
   <em>Note: You need to be signed in</em>
 </section> -->
 <article class="mollermethod">
-  <h1>A brand new <b>method</b>, coming soon.</h1>
-  <p>
-    mollermethod - the sequel to Luau Hub, will be miles ahead of our
-    competitors.
-  </p>
-  <h2>Compare</h2>
-  <table>
-    <thead>
-      <tr>
-        <th>Feature</th>
-        <th>Domain Hub X</th>
-        <th>mollermethod</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th>No keys</th>
-        <th>Premium</th>
-        <th>✔</th>
-      </tr>
-      <tr>
-        <th>Themes & Wallpapers</th>
-        <th>Premium</th>
-        <th>✔</th>
-      </tr>
-      <tr>
-        <th>Join codes</th>
-        <th>Premium</th>
-        <th>✔</th>
-      </tr>
-      <tr>
-        <th>Cloud Scripts</th>
-        <th>✖</th>
-        <th>✔</th>
-      </tr>
-      <tr>
-        <th>Anti-kick</th>
-        <th>✔</th>
-        <th>✔</th>
-      </tr>
-      <tr>
-        <th>Favorite pet</th>
-        <th>cta ✖</th>
-        <th>bragle ✔</th>
-      </tr>
-      <tr>
-        <th>Developers</th>
-        <th>&GreaterEqual;5</th>
-        <th>2</th>
-      </tr>
-    </tbody>
-  </table>
+  <h1>
+    The future of script hubs, coming soon. <sup>
+      <img src="/mollermethod.png" alt="mollermethod" height="16" />
+    </sup>
+  </h1>
 </article>
 
 <style>
-  table {
-    border: solid 1px white;
-    border-radius: 5px;
-  }
-  tbody tr:nth-child(odd) {
-    background: rgba(0, 0, 0, 0.5);
-  }
-  .mollermethod p {
-    max-width: 500px;
-  }
   .mollermethod {
-    background: url(mollermethod.png);
-    background-position: 700px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    width: max-content;
+    padding-left: 1em;
+    padding-right: 1em;
+    background-position: 100% 0;
+    background-size: contain;
+    background-color: black;
+    border-radius: 1em;
     background-repeat: no-repeat;
-    height: 700px;
+  }
+  .mollermethod h1 {
+    display: inline;
   }
   .dash {
     color: rgb(66, 155, 251);
